8663db4b1e541a6ecb7336aef994ef59b3956236
bsandoz <78384253+bsandoz@users.noreply.github.com>
2022-05-03 15:28:18 +0200

    2nd part memory game

--- src/components/gameComponents/Memory.vue
+++ src/components/gameComponents/Memory.vue
@@ -1,53 +1,22 @@
 <template>
   <h3>Test</h3>
   <div class="exercise">
-    <div class="memory-container">
-      <div class="memory-box">
-        <h4>Memory-Box 1</h4>
-      </div>
-      <div class="memory-box" @click="toggleVisibility" :class="{visible: isVisible}">
-        <h4>Memory-Box 2</h4>
-      </div>
-      <div class="memory-box">
-        <h4>Memory-Box 3</h4>
-      </div>
-      <div class="memory-box">
-        <h4>Memory-Box 4</h4>
-      </div>
-      <div class="memory-box">
-        <h4>Memory-Box 5</h4>
-      </div>
-      <div class="memory-box">
-        <h4>Memory-Box 6</h4>
-      </div>
-      <div class="memory-box">
-        <h4>Memory-Box 7</h4>
-      </div>
-      <div class="memory-box">
-        <h4>Memory-Box 8</h4>
-      </div>
-      <div class="memory-box">
-        <h4>Memory-Box 9</h4>
-      </div>
-      <div class="memory-box">
-        <h4>Memory-Box 10</h4>
-      </div>
-      <div class="memory-box">
-        <h4>Memory-Box 11</h4>
-      </div>
-      <div class="memory-box">
-        <h4>Memory-Box 12</h4>
-      </div>
+    <div class="memory-container" @click="startGame">
+      <h4 >Übung starten!</h4>
+
     </div>
   </div>
 </template>
 
 <script>
+import { h } from 'vue'
+
 export default {
   name: 'Memory',
   data() {
     return {
       isVisible: false,
+      isGameRunning: false,
     }
   },
   methods: {
@@ -58,7 +27,34 @@ export default {
         this.isVisible = true;
       }
     },
+    startGame() {
+      if (this.isGameRunning) {
+        console.log("Stopped game");
+        this.isGameRunning = false;
+      } else {
+        console.log("Started game");
+        this.isGameRunning = true;
+        const vnode = h(
+          'div',
+          { class: 'memory-box' },
+        )
+
+        /*
+        for (var i = 0; i < 12; i++) {
+          let memoryContainer = document.querySelector('.memory-container');
+          let memoryBox = document.createElement("div");
+          memoryBox.classList.add("memory-box")
+          memoryContainer.appendChild(memoryBox);
+        }
+        let memoryboxArray = document.getElementsByClassName('memory-box');
+        console.log(memoryboxArray);
+        */
+      }
+    }
   },
+  render() {
+    return h('div', { class: 'memory-box' })
+  }
 }
 </script>
 
@@ -85,3 +81,9 @@ export default {
     background-color: green;
   }
 </style>
+
+<!--
+<div class="memory-box" @click="toggleVisibility" :class="{visible: isVisible}">
+  <h4>Memory-Box</h4>
+</div>
+-->
