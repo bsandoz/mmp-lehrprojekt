c2d7d680a8f81192f7e31c118a13cc8a95f74521
bsandoz <78384253+bsandoz@users.noreply.github.com>
2022-05-04 14:34:41 +0200

    Memory game 1st implementation completed

--- src/components/gameComponents/Memory.vue
+++ src/components/gameComponents/Memory.vue
@@ -1,9 +1,14 @@
 <template>
-  <h3>Test</h3>
   <div class="exercise">
     <div class="memory-container" @click="startGame">
-      <h4 >Übung starten!</h4>
-
+      <h4 v-if="this.isGameRunning === false">Übung starten!</h4>
+      <div v-for="item in memoryboxArray" :key="item.id" v-if="this.isGameRunning">
+        <div class="memory-box" @click="toggleVisibility(item)"
+        :class="{visible: item.isVisible},{solved: item.isSolved}">
+          <h3>{{ item.id }}</h3>
+          <p>{{ item.fieldText }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,45 +20,105 @@ export default {
   name: 'Memory',
   data() {
     return {
-      isVisible: false,
       isGameRunning: false,
+      memoryboxArray: null,
+      visibilityCounter: 0,
+      stopRevealing: false,
+      firstBoxPair: 0,
+      secondBoxPair: 0,
+      firstBoxClicked: null,
+      secondBoxClicked: null,
     }
   },
   methods: {
-    toggleVisibility() {
-      if (this.isVisible) {
-        this.isVisible = false;
+    toggleVisibility(item) {
+      if (item.isSolved === false) {
+        if (item.isVisible) {
+          console.log("Already visible");
+        } else if (this.stopRevealing === false) {
+          if (this.visibilityCounter === 0) {
+            this.firstBoxPair = item.pair;
+            this.firstBoxClicked = item;
+            console.log(this.firstBoxPair);
+          }
+          if (this.visibilityCounter === 1) {
+            this.secondBoxPair = item.pair;
+            this.secondBoxClicked = item;
+            console.log(this.secondBoxPair);
+          }
+          item.isVisible = true;
+          this.visibilityCounter++;
+          if (this.visibilityCounter >= 2) {
+            this.stopRevealing = true;
+            this.compareBoxes(this.firstBoxClicked, this.secondBoxClicked);
+            setTimeout(this.makeBoxesInvisible, 3000);
+          }
+        } else {
+          console.log("Not allowed to reveal more boxes right now.");
+        }
       } else {
-        this.isVisible = true;
+        console.log("Clicked box is already solved");
       }
     },
-    startGame() {
-      if (this.isGameRunning) {
-        console.log("Stopped game");
-        this.isGameRunning = false;
-      } else {
-        console.log("Started game");
-        this.isGameRunning = true;
-        const vnode = h(
-          'div',
-          { class: 'memory-box' },
-        )
+    async startGame() {
+      try {
+        if (this.isGameRunning) {
+          await console.log("Already running game");
+        } else {
+          await console.log("Started game");
 
-        /*
-        for (var i = 0; i < 12; i++) {
-          let memoryContainer = document.querySelector('.memory-container');
-          let memoryBox = document.createElement("div");
-          memoryBox.classList.add("memory-box")
-          memoryContainer.appendChild(memoryBox);
+          await axios
+            .get ("https://ifuu2646.directus.app/items/memoryGameElements")
+            .then (response => (this.memoryboxArray = response.data.data))
+            .catch (function(error) {
+              console.log(error);
+            })
+          await this.shuffle(this.memoryboxArray);
+          this.isGameRunning = true;
         }
-        let memoryboxArray = document.getElementsByClassName('memory-box');
-        console.log(memoryboxArray);
-        */
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+    shuffle(array) {
+      let currentIndex = array.length,  randomIndex;
+
+      // While there remain elements to shuffle.
+      while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+
+      return array;
+    },
+    makeBoxesInvisible() {
+      for (var i = 0; i < this.memoryboxArray.length; i++) {
+        this.memoryboxArray[i].isVisible = false;
+      }
+      this.visibilityCounter = 0;
+      this.stopRevealing = false;
+      this.firstBoxPair = 0;
+      this.secondBoxPair = 0;
+      this.firstBoxClicked = null;
+      this.secondBoxClicked = null;
+    },
+    compareBoxes(firstBox, secondBox) {
+      if (this.firstBoxPair === this.secondBoxPair) {
+        firstBox.isSolved = true;
+        secondBox.isSolved = true;
       }
     }
   },
   render() {
-    return h('div', { class: 'memory-box' })
+    console.log("Render function called");
+    return h('div', this.msg)
   }
 }
 </script>
@@ -76,9 +141,15 @@ export default {
     border-style: solid;
     border-color: grey;
     margin-bottom: 50px;
+    font-size: 0;
   }
   .visible {
+    background-color: yellow;
+    font-size: 14pt;
+  }
+  .solved {
     background-color: green;
+    font-size: 14pt;
   }
 </style>
 
